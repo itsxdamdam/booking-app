0d253dee54d8bbe2fd92b774290d6eb1c6ed3cb4
thesaam <samuel@wedeydo.com>
2024-06-26 00:16:18 +0100

    refactor: redesign

--- src/App.js
+++ src/App.js
@@ -10,23 +10,46 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Co-working Space</h1>
-          <p>This is the number of available desks {numberOfDesk}</p>
+        <h1>Co-working Space</h1>
       </header>
-      <div className="cards">
-        <div className="card">
-          <input type="radio" />
-          <p>Basic $10 </p>
+      <div className="space-available-card">
+        <p className="space">Space Available</p>
+        <div className="inner-card">
+          <div className="small-cards">
+            <p className="number">15</p>
+            <p className="space-text">Desks</p>
+          </div>
+          <div className="small-cards">
+            <p className="number">10</p>
+            <p className="space-text">Individuals</p>
+          </div>
+          <div className="small-cards">
+            <p className="number">5</p>
+            <p className="space-text">Team Desks</p>
+          </div>
+          <div className="small-cards">
+            <p className="number">{numberOfDesk}</p>
+            <p className="space-text">Available</p>
+          </div>
         </div>
-        <div className="card">
-          <input type="radio" />
-          <p>Premium $15 </p>
+
+        <div className="tier-cards">
+          <div className="tier-card">
+            <input type="radio" />
+            <p>Basic $10 </p>
+          </div>
+          <div className="tier-card">
+            <input type="radio" />
+            <p>Premium $15 </p>
+          </div>
+          <div className="tier-card">
+            <input type="radio" />
+            <p>Executive $20 </p>
+          </div>
         </div>
-        <div className="card">
-          <input type="radio" />
-          <p>Executive $20 </p>
-        </div>
+
       </div>
+      
       <div className="buttons">
         <button className="book-now">Book</button>
       </div>
