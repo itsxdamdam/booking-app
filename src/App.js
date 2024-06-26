9dfb2325c6c20c827eab8eb16af2085051945439
thesaam <samuel@wedeydo.com>
2024-06-26 12:11:01 +0100

    feat: total displayed based on membership tier and number of hours

--- src/App.js
+++ src/App.js
@@ -3,22 +3,50 @@ import './App.css';
 
 function App() {
   const numberOfDesk = 15;
-  const basic = 10;
+  // const basic = 10;
+  // let amount = 0;
   const premium = 15;
   const executive = 20;
+
   
-  const [time, setTime] = useState(0)
+  const [time, setTime] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [checked, setChecked] = useState(false);
 
-
-  function handlePlus() {
+  const handlePlus = () => {
     setTime(time + 1)
   }
 
-  function handleMinus() {
-    if(time >= 1) {
+  // console.log(time)
+
+
+  const handleMinus = () => {
+    if(time !== 0) {
       setTime(time - 1)
     }
   }
+
+  const handleTier = (e) => {
+    setChecked(!checked)
+    if(e.target.value === "basic") {
+      setAmount(amount + 10)
+    } else if(e.target.value === "premium") {
+      setAmount(amount + 15)
+    } else {
+      setAmount(amount + 20)
+    }
+  }
+
+
+
+
+
+  const getTotal = () => {
+    // setTotal(time * basic)
+    // console.log(time * basic)
+  }
+
 
   // console.log(time)
 
@@ -63,15 +91,15 @@ function App() {
         <p className="tier-text">Membership Tier :</p>
         <div className="tier-cards">
           <div className="tier-card">
-            <input type="radio" />
+            <input type="radio" value={"basic"} onChange={handleTier}/>
             <p>Basic $10 </p>
           </div>
           <div className="tier-card">
-            <input type="radio" />
+            <input type="radio" value={"premium"} onChange={handleTier}/>
             <p>Premium $15 </p>
           </div>
           <div className="tier-card">
-            <input type="radio" />
+            <input type="radio" value={"executive"} onChange={handleTier} />
             <p>Executive $20 </p>
           </div>
         </div>
@@ -86,6 +114,8 @@ function App() {
             <button onClick={handlePlus}>+</button>
           </div>
         </div>
+
+        <p className="total">Total : ${time * amount} </p>
 
         <button className="book-btn">Book Desk</button>
 
